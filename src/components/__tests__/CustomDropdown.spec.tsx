2ad1c3f0c4617b56b9d8e7ecfc9a652ef8a85d3d
Nick Jansen <nick.jansen@mendix.com>
2021-05-10 15:40:05 +0200

    fix eslint problems

--- src/components/__tests__/CustomDropdown.spec.tsx
+++ src/components/__tests__/CustomDropdown.spec.tsx
@@ -1,17 +1,17 @@
-import { createElement } from 'react';
-import { render, fireEvent, waitFor } from '@testing-library/react';
+import { createElement } from "react";
+import { render, fireEvent } from "@testing-library/react";
 import { ValueStatus } from "mendix";
 
-import CustomDropdown from '../CustomDropdownComp';
+import CustomDropdown from "../CustomDropdownComp";
 
 const defaultValue = () => ({
     status: ValueStatus.Available,
     items: [
         {
-            firstLabel: { displayValue: 'label1' },
-            secondLabel: { displayValue: 'secondLabel1' },
-            imgUrl: { displayValue: 'url1' },
-        },
+            firstLabel: { displayValue: "label1" },
+            secondLabel: { displayValue: "secondLabel1" },
+            imgUrl: { displayValue: "url1" }
+        }
     ]
 });
 
@@ -19,29 +19,29 @@ const options = {
     status: ValueStatus.Available,
     items: [
         {
-            firstLabel: { displayValue: 'label1' },
-            secondLabel: { displayValue: 'secondLabel1' },
-            imgUrl: { displayValue: 'url1' },
+            firstLabel: { displayValue: "label1" },
+            secondLabel: { displayValue: "secondLabel1" },
+            imgUrl: { displayValue: "url1" }
         },
         {
-            firstLabel: { displayValue: 'label2' },
-            secondLabel: { displayValue: 'secondLabel2' },
-            imgUrl: { displayValue: 'url2' },
+            firstLabel: { displayValue: "label2" },
+            secondLabel: { displayValue: "secondLabel2" },
+            imgUrl: { displayValue: "url2" }
         },
         {
-            firstLabel: { displayValue: 'label3' },
-            secondLabel: { displayValue: 'secondLabel3' },
-            imgUrl: { displayValue: 'url3' },
-        },
+            firstLabel: { displayValue: "label3" },
+            secondLabel: { displayValue: "secondLabel3" },
+            imgUrl: { displayValue: "url3" }
+        }
     ]
 };
 
 const renderComponent = (override = {}) => {
     const props = {
         defaultValue: defaultValue(),
-        firstLabel: (item) => item.firstLabel,
-        secondLabel: (item) => item.secondLabel,
-        imgUrl: (item) => item.imgUrl,
+        firstLabel: item => item.firstLabel,
+        secondLabel: item => item.secondLabel,
+        imgUrl: item => item.imgUrl,
         options,
         contextObjLabel: {
             status: ValueStatus.Available,
@@ -52,58 +52,58 @@ const renderComponent = (override = {}) => {
         enableSearch: true,
         useAvatar: true,
         useDefaultStyle: true,
-        placeholder: 'placeholder',
-        className: 'custom-dropdown',
-        classNamePrefix: 'test',
+        placeholder: "placeholder",
+        className: "custom-dropdown",
+        classNamePrefix: "test",
         menuHeight: 0,
         ...override
     };
     // @ts-ignore
     return render(<CustomDropdown {...props} />);
-}
+};
 
-describe('Custom dropdown component', () => {
-    describe('and menu is shown', () => {
+describe("Custom dropdown component", () => {
+    describe("and menu is shown", () => {
         let container;
 
         beforeEach(() => {
             const component = renderComponent({ enableCreate: true });
             container = component.container;
 
-            const downButton = container.querySelector('div.test__dropdown-indicator');
+            const downButton = container.querySelector("div.test__dropdown-indicator");
 
             fireEvent.mouseDown(downButton, { button: 1 });
 
             // for printing the HTML
             // const { debug } = component;
             // debug();
-        })
+        });
 
-        it('- should render menu', () => {
-            const menu = container.querySelector('div.test__menu');
+        it("- should render menu", () => {
+            const menu = container.querySelector("div.test__menu");
             expect(menu).toBeDefined();
-        })
+        });
 
-        it('- should render menu items', () => {
-            const allAvatars = container.querySelectorAll('div.test__option > div.test__avatar');
+        it("- should render menu items", () => {
+            const allAvatars = container.querySelectorAll("div.test__option > div.test__avatar");
 
             expect(allAvatars).toHaveLength(3);
-            expect(allAvatars[0].style['background-image']).toContain('url1');
-            expect(allAvatars[1].style['background-image']).toContain('url2');
-            expect(allAvatars[2].style['background-image']).toContain('url3');
+            expect(allAvatars[0].style["background-image"]).toContain("url1");
+            expect(allAvatars[1].style["background-image"]).toContain("url2");
+            expect(allAvatars[2].style["background-image"]).toContain("url3");
 
-            const allNames = container.querySelectorAll('div.test__option > div.test__name');
+            const allNames = container.querySelectorAll("div.test__option > div.test__name");
             expect(allNames).toHaveLength(6);
-            expect(allNames[0].innerHTML).toBe('label1');
-            expect(allNames[1].innerHTML).toBe('secondLabel1');
-            expect(allNames[2].innerHTML).toBe('label2');
-            expect(allNames[3].innerHTML).toBe('secondLabel2');
-            expect(allNames[4].innerHTML).toBe('label3');
-            expect(allNames[5].innerHTML).toBe('secondLabel3');
-        })
-    })
+            expect(allNames[0].innerHTML).toBe("label1");
+            expect(allNames[1].innerHTML).toBe("secondLabel1");
+            expect(allNames[2].innerHTML).toBe("label2");
+            expect(allNames[3].innerHTML).toBe("secondLabel2");
+            expect(allNames[4].innerHTML).toBe("label3");
+            expect(allNames[5].innerHTML).toBe("secondLabel3");
+        });
+    });
 
-    describe('and clear button is pressed', () => {
+    describe("and clear button is pressed", () => {
         let component;
         const clearValue = jest.fn();
         const contextObjLabelSetValue = jest.fn();
@@ -124,24 +124,24 @@ describe('Custom dropdown component', () => {
                     status: ValueStatus.Available,
                     value: null,
                     setValue: contextObjIdSetValue
-                },
+                }
             });
 
-            const clearButton = component.container.querySelector('div.test__clear-indicator');
+            const clearButton = component.container.querySelector("div.test__clear-indicator");
             fireEvent.mouseDown(clearButton, { button: 0 });
-        })
+        });
 
-        it('- should show the placeholder', () => {
-            const placeholder = component.container.querySelector('div.test__value-container > div.test__placeholder');
+        it("- should show the placeholder", () => {
+            const placeholder = component.container.querySelector("div.test__value-container > div.test__placeholder");
             expect(placeholder).not.toBeNull();
         });
 
-        it('- should call clearValue action', () => {
+        it("- should call clearValue action", () => {
             expect(clearValue).toHaveBeenCalledTimes(1);
         });
     });
 
-    describe('New entry is created', () => {
+    describe("New entry is created", () => {
         let component;
         const createValue = jest.fn();
         const contextObjLabelSetValue = jest.fn();
@@ -162,30 +162,28 @@ describe('Custom dropdown component', () => {
                     status: ValueStatus.Available,
                     value: null,
                     setValue: contextObjIdSetValue
-                },
+                }
             });
 
-            const inputValue = component.container.querySelector('div.test__input > input');
-            fireEvent.change(inputValue , { target: { value: 'This is a new value' } });
-
-            
-        })
-
-        it('- should show option to create value', () => {
-            expect(component.container.querySelector('div.test__menu-list > div.test__option')!.textContent).toBe(
-                'Create "This is a new value"');
+            const inputValue = component.container.querySelector("div.test__input > input");
+            fireEvent.change(inputValue, { target: { value: "This is a new value" } });
         });
 
-        it('- should create a value', () => {
-            const createOption = component.container.querySelector('div.test__menu-list > div.test__option')
+        it("- should show option to create value", () => {
+            expect(component.container.querySelector("div.test__menu-list > div.test__option")!.textContent).toBe(
+                'Create "This is a new value"'
+            );
+        });
 
-            fireEvent.keyDown(createOption, { key: 'Enter', code: 'Enter'})
+        it("- should create a value", () => {
+            const createOption = component.container.querySelector("div.test__menu-list > div.test__option");
+
+            fireEvent.keyDown(createOption, { key: "Enter", code: "Enter" });
             expect(createValue).toHaveBeenCalledTimes(1);
         });
     });
 
-    
-    describe('Selection is changed', () => {
+    describe("Selection is changed", () => {
         let component;
         const selectOption = jest.fn();
         const contextObjLabelSetValue = jest.fn();
@@ -206,24 +204,23 @@ describe('Custom dropdown component', () => {
                     status: ValueStatus.Available,
                     value: null,
                     setValue: contextObjIdSetValue
-                },
+                }
             });
 
-            const inputValue = component.container.querySelector('div.test__input > input');
-            fireEvent.change(inputValue , { target: { value: 'label3' } });
-
-            
-        })
-
-        it('- should show value in menu', () => {
-            expect(component.container.querySelector('div.test__menu-list > div.test__option')!.textContent).toBe(
-                'label3secondLabel3');
+            const inputValue = component.container.querySelector("div.test__input > input");
+            fireEvent.change(inputValue, { target: { value: "label3" } });
         });
 
-        it('- should select the value', () => {
-            const createOption = component.container.querySelector('div.test__menu-list > div.test__option')
+        it("- should show value in menu", () => {
+            expect(component.container.querySelector("div.test__menu-list > div.test__option")!.textContent).toBe(
+                "label3secondLabel3"
+            );
+        });
 
-            fireEvent.keyDown(createOption, { key: 'Enter', code: 'Enter'})
+        it("- should select the value", () => {
+            const createOption = component.container.querySelector("div.test__menu-list > div.test__option");
+
+            fireEvent.keyDown(createOption, { key: "Enter", code: "Enter" });
             expect(selectOption).toHaveBeenCalledTimes(1);
         });
     });
