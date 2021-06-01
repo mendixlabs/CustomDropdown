33a9c6d5d60753dcf00a7e1c6bce64b3790eb968
Nick Jansen <nick.jansen@mendix.com>
2021-06-01 12:43:16 +0200

    fix unit tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,8 @@
 const base = require("@mendix/pluggable-widgets-tools/configs/eslint.ts.base.json");
 
 module.exports = {
-    ...base
+    ...base,
+    "rules": {
+        "@typescript-eslint/ban-ts-ignore": "off"
+      }
 };
